35d9dfa4a7ddd86153eb99329d37f0defac704aa
Lawrence Luo <luolawrence1@gmail.com>
2020-05-19 14:05:39 -0700

    initial web forms setup

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from app import web_app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import NumberForm
 
-@web_app.route('/')
+@web_app.route('/', methods=["GET", "POST"])
 def index():
-  return render_template('index.html')
+  form = NumberForm()
+  if form.validate_on_submit():
+    guess = form.bpm.data
+    flash('You submitted: {}'.format(guess))
+    return redirect('/') # redirect(url_for('index'))
+  return render_template('index.html', form=form)
